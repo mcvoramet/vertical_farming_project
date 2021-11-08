ce9be13c456476022e2ccb7a28ee30054c6f1cd6
mcvoramet <vc2543@gmail.com>
2021-11-08 17:59:47 +0700

    fix code

--- vertical_farming_project/vertical_farming_project.ino
+++ vertical_farming_project/vertical_farming_project.ino
@@ -1,13 +1,11 @@
 // Avoid use pin 0,1,3,6,7,8,9,10,11,12
 
-#include <WiFi.h>
-
-#define soil_pin_1 2
-#define soil_pin_2 4
-#define soil_pin_3 5
+#define soil_pin_1 14
+//#define soil_pin_2 4
+//#define soil_pin_3 5
 #define water_pin_1 13
-#define water_pin_2 14
-#define water_pin_3 15
+//#define water_pin_2 14
+//#define water_pin_3 15
 
 void setup() {
 
@@ -18,18 +16,27 @@ void setup() {
 }
 
 void loop() {
-  
-  float th = 150;
+
+  float th = 2500;
   float soil_moist_val = analogRead(soil_pin_1);
-  
-  if (soil_moist_val < th){
+
+  if (soil_moist_val > th){
     digitalWrite(water_pin_1, HIGH);
-    Serial.print("Water Pump ON");
-    delay(100);                   
+    delay(1000);
+    digitalWrite(water_pin_1, LOW);
+    Serial.println("Water Pump ON");
+    delay(2000);
+
   }
   
+  else if (soil_moist_val <= th){
+    digitalWrite(water_pin_1, LOW);
+    Serial.println("Water Pump OFF");
+
+  }
+
   Serial.print("Moisture Value = ");
-  Serial.println(analogRead(soil_pin_1);
+  Serial.println(analogRead(soil_pin_1));
   delay(100);
-  
+
 }
